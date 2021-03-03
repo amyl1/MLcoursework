bd19e10d0ae5f172c080ae142af4479061f11712
amyl1 <amyvlaws@gmail.com>
2021-03-03 10:10:16 +0000

    Remove rows with age NaN

--- prog.py
+++ prog.py
@@ -7,19 +7,18 @@ from sklearn.impute import SimpleImputer
 X_full = pd.read_csv("data.csv",low_memory=False)
 X_full['age_isDigit'] = np.where(X_full['age'].str.isdigit(), ['Yes'],['No'])
 for index, row in X_full.iterrows():
+    
     if row['age_isDigit']=="No" and '-' in row['age']:
-        print("changed")
         ages=row['age'].split('-')
         try:
+            #take age range and find median
             median=(int(ages[0])+int(ages[1]))/2
             row['age']=math.ceil(median)
         except:
             row['age']='NaN'
-
-print("finished")
-
+#remove rows with NaN
+X_full = X_full[X_full['age'].notna()]
 X_train, X_test = train_test_split(X_full, test_size=0.33)
-
 
 print(X_train.head())
 print(X_test.head())
